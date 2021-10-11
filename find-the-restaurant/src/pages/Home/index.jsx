8d744d29dee33e2ab3170cd6db646fe98991a10c
arthurfortunato <luizarthur.fortunato@gmail.com>
2021-10-11 15:52:30 -0300

    Desenvolvendo o Carousel com o React Slick

--- find-the-restaurant/src/pages/Home/index.jsx
+++ find-the-restaurant/src/pages/Home/index.jsx
@@ -1,31 +1,53 @@
+import Slider from "react-slick";
 import { useState } from 'react';
 
 import TextField, { Input } from '@material/react-text-field';
+import MaterialIcon from '@material/react-material-icon';
 
 import logo from '../../assets/logo.svg';
 
-import { Container, Search } from "./styles";
+import { Wrapper, Container, Search, Logo, CarouselTitle } from "./styles";
 
 
 const Home = () => {
     const [inputValue, setInputValue] = useState('');
-        return (
-        <Container>
-            <Search>
-                <img src={logo} alt="logo do restaurante" />
-                <TextField
-                    label='Pesquisar'
-                    outlined
-                //onTrailingIconSelect={() => this.setState({value: ''})}
-                //trailingIcon={<MaterialIcon role="button" icon="delete"/>}
-                >
-                    <Input
-                        value={inputValue}
-                        onChange={(e) => setInputValue(e.target.value)} />
-                </TextField>
-            </Search>
-        </Container>
-        );
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        adaptiveHeight: true,
+      };
+
+    return (
+        <Wrapper>
+            <Container>
+                <Search>
+                    <Logo src={logo} alt="logo do restaurante" />
+                    <TextField
+                        label='Pesquisar Restaurantes'
+                        outlined
+                        trailingIcon={<MaterialIcon role="button" icon="search" />}>
+                        <Input
+                            value={inputValue}
+                            onChange={(e) => setInputValue(e.target.value)} />
+                    </TextField>
+                    <CarouselTitle>Perto de você!</CarouselTitle>
+                    <Slider {...settings}>
+                        <div><h3>1</h3></div>
+                        <div><h3>1</h3></div>
+                        <div><h3>1</h3></div>
+                        <div><h3>1</h3></div>
+                        <div><h3>1</h3></div>
+                        <div><h3>1</h3></div>
+                        <div><h3>1</h3></div>
+                    </Slider>
+                </Search>
+            </Container>
+        </Wrapper>
+    );
 };
 
 export default Home;
--- find-the-restaurant/src/pages/Home/styles.js
+++ find-the-restaurant/src/pages/Home/styles.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
+export const Wrapper = styled.div`
+  display:flex;
+`;
+
 export const Container = styled.aside`
   background-color: ${(propos) => propos.theme.colors.background};
   width: 360px;
@@ -14,3 +18,17 @@ export const Search = styled.section`
     background-color: #ffffff;
     padding: 16px;
 `;
+
+export const Logo = styled.img`
+  margin-bottom: 15px;
+  height: 75px
+`;
+
+export const CarouselTitle = styled.h1`
+  font-family: ${(props) => props.theme.fonts.regular};  
+  color: ${(props) => props.theme.colors.text};
+  font-size: 24px;
+  font-weight: bold;
+  line-height: 29px;
+  margin: 16px 0;
+  `;
